6596e103fd870c61c18e318b11e7451516b588b7
Carlos Mendible <cmendible@gmail.com = Carlos Mendible cmendible@gmail.com>
2011-04-28 11:16:00 +0000

    Adding netDumbster's NuGet sample package project... (Applied more of patch 9107)

---
+++ netDumbster.Sample/TxTest.cs
@@ -0,0 +1,98 @@
+//
+using System;
+using System.Net;
+using System.Net.Mail;
+//
+using NUnit.Framework;
+//
+using netDumbster.smtp;
+
+namespace Samples.netDumbster
+{
+
+    [TestFixture]
+    public class TxNetDumbsterSamplesTest
+    {
+        private static SimpleSmtpServer _Server;
+        private Random _Rnd = new Random();
+
+        public TxNetDumbsterSamplesTest()
+        {
+            log4net.Config.XmlConfigurator.Configure();
+        }
+
+        private void SendMail()
+        {
+            SendMail(false);
+        }
+
+        private void SendMail(bool smtpAuth)
+        {
+            SmtpClient client = new SmtpClient("localhost", _Server.Port);
+            var mailMessage = new MailMessage("cfm@mendible.com", "kbm@mendible.com", "test", "test tes test");
+
+            if (smtpAuth)
+            {
+                NetworkCredential credentials = new NetworkCredential("user", "pwd");
+                client.Credentials = credentials;
+                client.EnableSsl = false;
+            }
+
+            client.Send(mailMessage);
+        }
+
+        [TestFixtureSetUp]
+        public void FixtureSetUp()
+        {
+            _Server = SimpleSmtpServer.Start(_Rnd.Next(50000, 60000));
+        }
+
+        [TestFixtureTearDown]
+        public void FixtureTearDown()
+        {
+            _Server.Stop();
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            _Server.ClearReceivedEmail();
+        }
+
+        [Test]
+        public void Send_One_Mail()
+        {
+            SendMail();
+            Assert.AreEqual(1, _Server.ReceivedEmailCount);
+        }
+
+        [Test]
+        public void Send_One_Mail_With_SmtpAuth()
+        {
+            SendMail(true);
+            Assert.AreEqual(1, _Server.ReceivedEmailCount);
+        }
+
+        [Test]
+        public void Send_100_Mail_With_SmtpAuth()
+        {
+            for (var i = 0; i < 100; i++)
+            {
+                SendMail(true);
+                Assert.AreEqual(i + 1, _Server.ReceivedEmailCount);
+            }
+            Assert.AreEqual(100, _Server.ReceivedEmailCount);
+        }
+
+        [Test]
+        public void Send_100_Mails()
+        {
+            for (var i = 0; i < 100; i++)
+            {
+                SendMail();
+                Assert.AreEqual(i + 1, _Server.ReceivedEmailCount);
+            }
+            Assert.AreEqual(100, _Server.ReceivedEmailCount);
+        }
+    }
+}
---
+++ packNetDumbsterSampleNuGetPackage.bat
@@ -0,0 +1,1 @@
+"libs/NuGet/NuGet.exe" pack netDumbster.Sample/NuGet/netDumbster.Sample.nuspec -b "netDumbster.Sample" -o "netDumbster.Sample/NuGet/"
