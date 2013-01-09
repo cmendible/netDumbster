2ef0aab71ec1bab0e776cd91e9e521dc51f40e67
cmendible <carlos.mendible@hexasystems.com>
2013-01-09 12:27:57 +0100

    Completed simple ConsoleLogger

--- netDumbster/Logging/ConsoleLogger.cs
+++ netDumbster/Logging/ConsoleLogger.cs
@@ -4,95 +4,140 @@
 
     public class ConsoleLogger : ILog
     {
-        Type type;
+        private string type;
+        private const string layout = "{0} - {1} - {2} - {3}";
 
         public ConsoleLogger(Type type)
         {
-            this.type = type;
+            this.type = type.FullName;
         }
 
         #region Methods
 
         public void Debug(object message)
-        { 
+        {
+            WriteToConsole("DEBUG", message);
         }
 
         public void Debug(object message, Exception exception)
-        { 
+        {
+            WriteToConsole("DEBUG", message, exception);
         }
 
         public void DebugFormat(string format, params object[] args)
-        { 
+        {
+            WriteToConsole("DEBUG", format, args);
         }
 
         public void DebugFormat(IFormatProvider provider, string format, params object[] args)
-        { 
+        {
+            WriteToConsole("DEBUG", provider, format, args);
         }
 
         public void Error(object message)
-        { 
+        {
+            WriteToConsole("ERROR", message);
         }
 
         public void Error(object message, Exception exception)
-        { 
+        {
+            WriteToConsole("ERROR", message, exception);
         }
 
         public void ErrorFormat(string format, params object[] args)
-        { 
+        {
+            WriteToConsole("ERROR", format, args);
         }
 
         public void ErrorFormat(IFormatProvider provider, string format, params object[] args)
-        { 
+        {
+            WriteToConsole("ERROR", provider, format, args);
         }
 
         public void Fatal(object message)
-        { 
+        {
+            WriteToConsole("FATAL", message);
         }
 
         public void Fatal(object message, Exception exception)
-        { 
+        {
+            WriteToConsole("FATAL", message, exception);
         }
 
         public void FatalFormat(string format, params object[] args)
-        { 
+        {
+            WriteToConsole("FATAL", format, args);
         }
 
         public void FatalFormat(IFormatProvider provider, string format, params object[] args)
-        { 
+        {
+            WriteToConsole("FATAL", provider, format, args);
         }
 
         public void Info(object message)
-        { 
+        {
+            WriteToConsole("INFO", message);
         }
 
         public void Info(object message, Exception exception)
-        { 
+        {
+            WriteToConsole("INFO", message, exception);
         }
 
         public void InfoFormat(string format, params object[] args)
-        { 
+        {
+            WriteToConsole("INFO", format, args);
         }
 
         public void InfoFormat(IFormatProvider provider, string format, params object[] args)
-        { 
+        {
+            WriteToConsole("INFO", provider, format, args);
         }
 
         public void Warn(object message)
-        { 
+        {
+            WriteToConsole("WARNING", message);
         }
 
         public void Warn(object message, Exception exception)
-        { 
+        {
+            WriteToConsole("WARNING", message, exception);
         }
 
         public void WarnFormat(string format, params object[] args)
-        { 
+        {
+            WriteToConsole("WARNING", format, args);
         }
 
         public void WarnFormat(IFormatProvider provider, string format, params object[] args)
-        { 
+        {
+            WriteToConsole("WARNING", provider, format, args);
         }
 
+        private void WriteToConsole(string level, object message)
+        {
+            Console.WriteLine(string.Format(layout, CurrentDateTime(), level, this.type, message));
+        }
+
+        private void WriteToConsole(string level, object message, Exception exception)
+        {
+            Console.WriteLine(string.Format(layout, CurrentDateTime(), level, this.type, string.Format("{0} Exception: {1}", message, exception.ToString())));
+        }
+
+        private void WriteToConsole(string level, string format, params object[] args)
+        {
+            Console.WriteLine(string.Format(layout, CurrentDateTime(), level, this.type, string.Format(format, args)));
+        }
+
+        private void WriteToConsole(string level, IFormatProvider provider, string format, params object[] args)
+        {
+            Console.WriteLine(string.Format(layout, CurrentDateTime(), level, this.type, string.Format(provider, format, args)));
+        }
+
+        private static string CurrentDateTime()
+        {
+            return DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss.fff");
+        }
         #endregion Methods
     }
 }
