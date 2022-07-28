2846614174789870ab938f178400bd19bff9b283
Stefan Schweiger <staeff@me.com>
2022-07-28 11:21:01 +0200

    Fixed placement of import

--- netDumbster.Test/TestsBase.cs
+++ netDumbster.Test/TestsBase.cs
@@ -1,10 +1,9 @@
-﻿using System.Linq;
-
-namespace netDumbster.Test
+﻿namespace netDumbster.Test
 {
     using System;
     using System.Collections.Generic;
     using System.IO;
+    using System.Linq;
     using System.Net;
     using System.Text;
     using netDumbster.smtp;
